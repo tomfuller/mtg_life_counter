9a3d53e043733e75c85a3170037ab6c03784c38b
Tom Fuller <tomf95@hotmail.co.uk>
2017-02-15 12:45:26 +0000

    logic for energy counter added

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -19,4 +19,24 @@ describe("Player", function() {
     player.increaseLifeBy1();
     expect(player.playerLife).toEqual(21);
   });
+
+  it("should be initialized with an energy count of 0", function() {
+    expect(player.energyCount).toEqual(0);
+  });
+
+  it("should be able to decrease energy by 1", function() {
+    player.energyCount = 1;
+    player.decreaseEnergyBy1();
+    expect(player.energyCount).toEqual(0);
+  });
+
+  it("should be able to increse energy by 1", function() {
+    player.increaseEnergyBy1();
+    expect(player.energyCount).toEqual(1);
+  });
+
+  it("shouldn't reduce energy into the negative", function() {
+    expect( function(){player.decreaseEnergyBy1(); }).toThrowError('Energy is already 0');
+    expect(player.energyCount).toEqual(0);
+  })
 });
--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,6 @@
 function Player() {
   this.playerLife = 20;
+  this.energyCount = 0;
 }
 
 Player.prototype.decreaseLifeBy1 = function() {
@@ -8,4 +9,17 @@ Player.prototype.decreaseLifeBy1 = function() {
 
 Player.prototype.increaseLifeBy1 = function() {
   this.playerLife += 1;
+};
+
+Player.prototype.decreaseEnergyBy1 = function() {
+  if(this.energyCount > 0) {
+    this.energyCount -= 1;
+    }
+  else {
+    throw Error("Energy is already 0")
+  }
+};
+
+Player.prototype.increaseEnergyBy1 = function() {
+  this.energyCount += 1;
 };
